fc516c8fc65577d9087901e5a4b8fbdfb6bf6d08
kevinrabinovich <githubcommits@kevinrabinovich.me>
2014-03-27 19:15:10 -0400

    Actually updated files to latest version of afaqurk/linux-dash
    Copied and pasted the correct files from afaqurk/linux-dash commit
    0ff78c218915daf3f84ae7f619e53ba6ee1ef593.

--- js/base.js
+++ js/base.js
@@ -1,91 +1,224 @@
-$(function () {
-	
-	
-	$('.subnavbar').find ('li').each (function (i) {
-	
-		var mod = i % 3;
-		
-		if (mod === 2) {
-			$(this).addClass ('subnavbar-open-right');
-		}
-		
-	});
-	
+$(document).ready(function() {
+    // keep widgets ordered
+    keepWidgetOrdered();
 
-/* ADDED BY AFAQ */	
-	
-var pulse_counter; // number of times the element should pulsate
-var pulse_interval; // interval (in milliseconds) of pulsating
+    // hide localstored hidden widgets
+    keepWidgetHidden();
 
+    // enable popovers
+    $(".pop").popover();
 
-function pulsate_element(element){
-    
-    while ( pulse_counter>0){
-        pulse_counter--;
-        setTimeout(function(){ element.parent().toggleClass('pulse'); },pulse_interval*pulse_counter);
-        setTimeout(function(){ element.parent().parent().toggleClass('pulse-boder'); },pulse_interval*pulse_counter);
-        pulsate_element(element);
+   // activate tooltips on hover
+   $("[data-toggle='tooltip']").tooltip({trigger: 'hover', placement:'right'});  
+
+    dashboard.getAll();
+}).on("click", ".js-smoothscroll", function(event) {
+    event.preventDefault();
+    var target = $(this.hash).parent();
+    pulseElement(target, 8, 400);
+
+    $("html,body").animate({
+        scrollTop: target.offset().top - 130
+    }, 1000);
+}).on("click", ".js-refresh-info", function(event) {
+    event.preventDefault();
+    var target = event.target;
+    var item = target.id.split("-").splice(-1)[0];
+
+    // if the refresh icon is click (where in a <span>) target will not have an id, so grab its parent instead
+    if(target.id == "") {
+        var parent = $(target).parent()[0];
+        item = parent.id.split("-").splice(-1)[0];
     }
-    
-    //if (pulse_counter==0){ pulse_counter=8;}
+
+    dashboard.fnMap[item]();
+});
+
+// Handle for cancelling active effect.
+var pulsing = {
+    element: null,
+    timeoutIDs: [],
+    resetfn: function() {
+        pulsing.element = null;
+        pulsing.timeoutIDs = [];
+    }
+};
+
+/**
+ * Applies a pulse effect to the specified element. If triggered while already
+ * active the ongoing effect is cancelled immediately.
+ *
+ * @param {HTMLElement} element The element to apply the effect to.
+ * @param {Number} times How many pulses.
+ * @param {Number} interval Milliseconds between pulses.
+ */
+function pulseElement(element, times, interval) {
+    if (pulsing.element) {
+        pulsing.element.removeClass("pulse").
+            parent().removeClass("pulse-border");
+        pulsing.timeoutIDs.forEach(function(ID) {
+            clearTimeout(ID);
+        });
+        pulsing.timeoutIDs = [];
+    }
+    pulsing.element = element;
+    var parent = element.parent();
+    var f = function() {
+        element.toggleClass("pulse");
+        parent.toggleClass("pulse-border");
+    };
+
+    pulsing.timeoutIDs.push(setTimeout(pulsing.resetfn,
+                                       (times + 1) * interval));
+    for (; times > 0; --times) {
+        pulsing.timeoutIDs.push(setTimeout(f, times * interval));
+    }
 }
 
-// smooth scrolling for links
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
+/**
+ * Adds jQuery UI sortable portlet functionality to widgets
+ *
+ *
+ */
 
-      var target = $(this.hash);
-      
-      pulse_counter=8; // number of times the element should pulsate
-      pulse_interval=400; // interval (in milliseconds) of pulsating
-      pulsate_element(target);
-      
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.parent().offset().top-130
-        }, 1000);
-        return false;
-      }
+$( "#widgets" ).sortable({
+      handle: ".widget-header",
+      cancel: "#filter-ps",
+      cursor: "move",
+      opacity: 0.7,
+      scrollSensitivity:10,
+      tolerance: 'pointer',
+      stop: function(event, ui) {
+            // save widget order in localstorage
+            var newOrder = new Array();
+            $('.widget').each(function() {
+                newOrder.push($(this).attr("id"));
+            });
+            localStorage.setItem('positions', JSON.stringify(newOrder));
+        }
+ });
+
+/**
+ *
+ * Widget hide functionality
+ *
+**/
+// general cached DOM objects
+closedWidgetCount = $('#closed-widget-count'),
+closedWidgets = $('#closed-widget-list');
+allWidgets = $('.widget');
+
+// Close all widgets
+$('#close-all-widgets').click(function(){
+    allWidgets.each(function(index){
+        if ($(this).is(":visible")){
+	       hideWidget($(this), 400);
+        }
+    });
+});
+
+// Open all widgets
+$('#open-all-widgets').click(function(){
+    allWidgets.each(function(index){
+	   openWidget($(this), $(this).attr('id'), 500);
+    });
+});
+
+// attach a close button to all widget headers
+$('.widget-header').append('<div class="btn btn-icon-only icon-remove hide-widget"></div>');
+
+// hide / close widget function
+$('.hide-widget').live('click',function(){
+    var widget = $(this).parent().parent();
+    hideWidget(widget, 300);
+});
+
+// unhide closed widget
+$('.open-widget').live('click',function(){
+    // cache DOM objects/data used in this function
+    var widgetIdentifier = $(this).data('id');
+    var widget = $( "#" + widgetIdentifier );
+    var navItem = $(this).parent();
+
+    openWidget(widget,widgetIdentifier,500);
+
+    // remove item from closed-widget-list
+    navItem.remove();
+
+});
+
+function openWidget(widget, widgetIdentifier, speed){
+
+    // decrement closed-widget-count 
+    if(widget.is(":hidden")) {
+        closedWidgetCount.text( Number(closedWidgetCount.text()) - 1);
     }
-  });
-});
 
-/* Added by kevinrabinovich */
-	//Adds <meter> element displaying % disk used
-	//Tests if the following has already been run, because it runs twice otherwise		
-	var progressPresent = document.getElementsByTagName("progress");
-	var meterPresent = document.getElementsByTagName("meter");
-	var progressBarClassPresent = document.getElementsByClassName("progress-bar");
-	if ((!(progressPresent.length > 0) || (!(meterPresent.length > 0)) || (!(progressBarClassPresent.length > 0))) /*If progress, meter, or .progress-bar doesn't exist*/) {
-		var table = document.getElementById('df_dashboard'); //Table
-		var rows = table.rows.length; //All rows
-		for (i = 1; i < rows; i++){
-			//For each row
-			var tcells = table.rows.item(i).cells; //All cells in this row
-			var usedTotal = 0;
-			var totalUsedLength = tcells.item(4).innerHTML.length - 1; //Length of characters in cell; subtracting 1 to account for '%'
-			for (j = totalUsedLength - 1; j >= 0; j--) { //For each place digit j in number; subtracting 1 because index is 0-based
-				if (totalUsedLength == 1) { //If there are only units, i.e. no tens
-					usedTotal += parseInt(tcells.item(4).innerHTML[j]);
-				}
-				else {
-					switch (j) {
-						case 1: //units
-							usedTotal += parseInt(tcells.item(4).innerHTML[j]);
-							break;
-						case 0: //tens
-							usedTotal += 10*parseInt(tcells.item(4).innerHTML[j]);
-							break;
-						default:
-							break;
-					}
-				}
-			}
-			tcells.item(0).innerHTML += "<meter max=100" + " value=" + usedTotal + " ><progress " + " position=" + usedTotal/100 + " value=" + usedTotal/100 + " >" + "<div class=progress-bar><span style=width:" + usedTotal + "% ></span></div></progress></meter>";
-		//End for each row
-		}
-	}
-});
+    // unhide widget
+    widget.show(500);
+
+     // remove widget from localstorage
+    var localData = JSON.parse(window.localStorage.getItem('hidden'));
+    for(var i = localData.length; i--;){
+        if (localData[i] == widgetIdentifier) {
+            localData.splice(i, 1);
+        }
+    }
+    localStorage.setItem('hidden', JSON.stringify(localData));
+}
+
+
+function hideWidget(widget, speed){
+    // cache DOM objects/data used in this function
+    var widgetName = widget.find('.widget-header h3').text();
+    var widgetIdentifier = widget.attr('id'); 
+
+    // update count
+    if(!widget.is(":hidden")) {
+        closedWidgetCount.text( Number(closedWidgetCount.text()) + 1);
+    }
+
+    // hide widget from DOM
+    widget.hide(speed);
+
+    // add to hidden list
+    closedWidgets.append('<li><a class="open-widget" data-id="'+widgetIdentifier+'"><i class="icon-plus-sign"></i>  '+widgetName+'</a></li>');
+
+    // add widget to localstorage (and create item if needed)
+    var localData = JSON.parse(window.localStorage.getItem('hidden'));
+    if(localData == null) {
+        hidden = new Array();
+        hidden.push(widgetIdentifier);
+        localStorage.setItem('hidden', JSON.stringify(hidden));
+    }
+    else{
+        if (!isInArray(localData, widgetIdentifier)) {
+            localData.push(widgetIdentifier);
+            localStorage.setItem('hidden', JSON.stringify(localData));
+        }
+    }
+}
+
+function keepWidgetHidden(){
+    var localData = JSON.parse(window.localStorage.getItem('hidden'));
+    if(localData!=null) {
+        $.each(localData, function(i,value){
+             hideWidget( $("#" + value), 0 );
+        });
+    }
+}
+
+function keepWidgetOrdered(){
+    var localData = JSON.parse(window.localStorage.getItem('positions'));
+    if(localData!=null) {
+        $.each(localData, function(i,value){
+            var widgetId ="#" + value;
+            $("#widgets").append($(widgetId).parent());
+        });
+    }
+}
+
+function isInArray(array, search)
+{
+    return (array.indexOf(search) >= 0) ? true : false; 
+}
--- sh/speed.php
+++ sh/speed.php
@@ -1,21 +1,151 @@
 <?php
 
-const MB = 0;
-const KB = 1;
+$mb = 1000000;
 
-$target = 'http://speedtest.pixelwolf.ch/one.gigabyte';
+/*
+ * Configure download & upload hosts.
+ * This array exist just to give some choice and flexibility to the user.
+ */
+$hosts = array(
+    /*
+     * A /dev/null type service to POST blobs which will discard data after reading is recommended.
+     * If you know of such service please let me know.
+     * This one here has some very cool features. Visit the site for more options like custom server status_code response etc.
+     * Change or remove resource query string dir=... if you do not wont your request logged.
+     */
+    'upload' => array(
+        0 => array(
+            'hostname' => 'posttestserver.com',
+            'resource' => '/post.php?dir=linuxdash',
+            'size' => 1, //MB
+            'timeout' => 30,
+        ),
+    ),
+    // A big(~10MB) CDN hosted( e.g CloudFlare ) blob would be preferable for high availability and consistent performance.
+    'download' => array(
+        0 => array(
+            /* IPv4 & IPv6 Port: 80, 81, 8080 IPv6 */
+            'hostname' => 'download.thinkbroadband.com',
+            'resource' => '/10MB.zip',
+            'size' => 10, //MB
+            'timeout' => 30,
+        ),
+        1 => array(
+            'hostname' => 'cachefly.cachefly.net',
+            'resource' => '/10mb.test',
+            'size' => 10, //MB
+            'timeout' => 30,
+        ),
+        2 => array(
+            /* Download 10MB file from Seattle */
+            'hostname' => 'speedtest.sea01.softlayer.com',
+            'resource' => '/downloads/test10.zip',
+            'size' => 10, //MB
+            'timeout' => 30,
+        ),
+        3 => array(
+            /* 50 MB */
+            'hostname' => 'download.thinkbroadband.com',
+            'resource' => '/50MB.zip',
+            'size' => 50, //MB
+            'timeout' => 120,
+        ),
+    ),
+);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $target);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_TIMEOUT, $timeout = 10);
+$dChosenHost = 1;
+$dHostname = $hosts['download'][$dChosenHost]['hostname'];
+$dResource = $hosts['download'][$dChosenHost]['resource'];
+$dSize = $hosts['download'][$dChosenHost]['size'] * $mb;
+$dTimeout = $hosts['download'][$dChosenHost]['timeout'];
+$dPort = 80;
 
-curl_exec($ch);
-$speed = curl_getinfo($ch, CURLINFO_SPEED_DOWNLOAD);
+$outDownload = "GET {$dResource} HTTP/1.1\r\n";
+$outDownload .= "Host: {$dHostname}\r\n";
+$outDownload .= "Connection: Close\r\n\r\n";
+$chunkSize = 1024;
 
-$as = isset($_GET['as']) ? (int) $_GET['as'] : MB;
+$endDownload = null;
+$speedDownstream = 0;
 
-$as == KB 
-  ? printf("%0.2f\n", ($speed / 1024))
-  : printf("%.1f\n", (($speed * 8) / 1024) / 1024);
+$strFile = '';
+$realDownloadSize = 0;
+$startDownload = microtime(true);
+
+/*timeout here only applies while connecting the socket*/
+$fpDownload = fsockopen($dHostname, $dPort, $errno, $errstr, $dTimeout);
+if (!$fpDownload) {
+    echo "$errstr ($errno)<br />\n";
+} else {
+    fwrite($fpDownload, $outDownload);
+    /* timeout here applies for reading/writing data over the socket */
+    stream_set_timeout($fpDownload, $dTimeout);
+    while (!feof($fpDownload)) {
+        $strFile .= fread($fpDownload, $chunkSize);
+    }
+    $endDownload = microtime(true);
+    fclose($fpDownload);
+    /*
+     * $realDownloadSize is calculated to make sure we are estimating the speed on the real download size.
+     * If the connection goes down before completing the download we will still be able to calculate the speed
+     * correctly based on the partial download.
+     * There is some confusion between MB and MiB too. Many blobs declared as x MB seems to be x MiB.
+     */
+    $realDownloadSize = strlen($strFile);
+    $timeDownload = $endDownload - $startDownload;
+    if ($timeDownload) {
+        $speedDownstream = ($realDownloadSize / $timeDownload);
+    }
+}
+
+// Proceed to upload only if we actually have something to do upload
+if ($realDownloadSize) {
+
+    $uChosenHost = 0;
+    $uHostname = $hosts['upload'][$uChosenHost]['hostname'];
+    $uResource = $hosts['upload'][$uChosenHost]['resource'];
+    $uSize = $hosts['upload'][$uChosenHost]['size'] * $mb;
+    $uTimeout = $hosts['upload'][$uChosenHost]['timeout'];
+    $uPort = 80;
+
+    $outUpload = "POST {$uResource} HTTP/1.1\r\n";
+    $outUpload .= "Host: {$uHostname}\r\n";
+    $outUpload .= "Content-type: application/x-www-form-urlencoded\r\n";
+    $outUpload .= "Content-length: " . $realDownloadSize . "\r\n";
+    $outUpload .= "Accept: */*\r\n\r\n";
+    $data = urlencode($strFile);
+
+    $endUpload = null;
+    $speedUpstream = 0;
+
+    $startUpload = microtime(true);
+
+    $fpUpload = fsockopen($uHostname, $uPort, $errno, $errstr, $uTimeout);
+    if (!$fpUpload) {
+        echo "$errstr ($errno)<br />\n";
+    } else {
+        fwrite($fpUpload, $outUpload);
+        stream_set_timeout($fpUpload, $uTimeout);
+        $writeSize = fwrite($fpUpload, $data, $uSize);
+
+        if ($writeSize) {
+            $endUpload = microtime(true);
+            fclose($fpUpload);
+
+            $timeUpload = $endUpload - $startUpload;
+            if ($timeUpload) {
+                $speedUpstream = ($writeSize / $timeUpload);
+            }
+
+        }
+    }
+}
+
+$speed = array(
+    'upstream' => $speedUpstream,
+    'downstream' => $speedDownstream,
+);
+
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($speed);
+
